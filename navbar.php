cc3fda4b4912447946505b6550319bd5b1bdcc16
Shamritha27 <sharithredss@gmail.com>
2021-06-14 13:54:43 +0530

    navbar change

--- navbar.php
+++ navbar.php
@@ -2,11 +2,11 @@
 <link rel="stylesheet" href="navbar.css">
 
 
-<div class="navbar">
+<div class="navbar" style="width:100%">
        <i class="fa fa-university icon fa-2x" style="height:43px;color:white;"></i>
        <h2 style="color:white;padding-left:10px;font-family: serif;">The Sparks Bank</h2>
 
-    <ul class="cul ml-auto" style="width:100%">
+    <ul class="cul ml-auto">
         <li class="cli">
             <a class="text" href="index.php">Home</a>
         </li>
